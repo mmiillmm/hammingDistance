ceaac4d60a88fb569934f72037b94c038d629f00
Makai Kristof <kmakai520@gmail.com>
2023-01-19 13:15:06 +0100

    Add project files.

---
+++ hammingDistance/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hammingDistance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int szam = 0;
+
+            Console.WriteLine("Kérem adjon meg két azonos hosszúságú szöveget: ");
+
+            Console.Write("Első szöveg: ");
+
+            string elsoSzoveg = Console.ReadLine();
+
+            Console.Write("Második szöveg: ");
+
+            string masodikSzoveg = Console.ReadLine();
+
+            for (int i = 0; i < elsoSzoveg.Length; i++)
+            {
+                if (elsoSzoveg[i] != masodikSzoveg[i])
+                {
+                    szam++;
+                }
+            }
+
+            Console.WriteLine($"A {elsoSzoveg} és {masodikSzoveg} távolsága: {szam}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
